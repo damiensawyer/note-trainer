851579d2754f233dce169be5724d9128f1e519af
Damien Sawyer <damiensawyer@gmail.com>
2022-03-07 21:09:59 +0000

    wired up midi

---
+++ src/lib/Midi.svelte
@@ -0,0 +1,100 @@
+﻿<script lang="ts">
+
+    // from https://webmidi-examples.glitch.me/
+    import {onMount} from "svelte";
+
+    let midiIn = [];
+    let midiOut = [];
+    let notesOn = new Map();
+
+    function connect() {
+        console.log('connecting to midi...');
+        navigator.requestMIDIAccess()
+            .then(
+                (midi) => midiReady(midi),
+                (err) => console.log('Something went wrong', err));
+    }
+
+    function midiReady(midi) {
+        // Also react to device changes.
+        console.log('connected to midi');
+        
+        initDevices(midi);
+        midi.addEventListener('statechange', (event) => initDevices(event.target));
+    }
+
+    function initDevices(midi) {
+        // Reset.
+        midiIn = [];
+        midiOut = [];
+
+        // MIDI devices that send you data.
+        const inputs = midi.inputs.values();
+        for (let input = inputs.next(); input && !input.done; input = inputs.next()) {
+            midiIn.push(input.value);
+        }
+
+        // MIDI devices that you send data to.
+        const outputs = midi.outputs.values();
+        for (let output = outputs.next(); output && !output.done; output = outputs.next()) {
+            midiOut.push(output.value);
+        }
+        console.log('midi in', midiIn)
+        console.log('midi out', midiOut)
+//    displayDevices();
+        startListening();
+    }
+
+
+    // Start listening to MIDI messages.
+    function startListening() {
+        for (const input of midiIn) {
+            input.addEventListener('midimessage', midiMessageReceived);
+        }
+    }
+
+
+    function midiMessageReceived(event) {
+        // MIDI commands we care about. See
+        // http://webaudio.github.io/web-midi-api/#a-simple-monophonic-sine-wave-midi-synthesizer.
+        const NOTE_ON = 9;
+        const NOTE_OFF = 8;
+
+        const cmd = event.data[0] >> 4;
+        const pitch = event.data[1];
+        const velocity = (event.data.length > 2) ? event.data[2] : 1;
+
+        // You can use the timestamp to figure out the duration of each note.
+        const timestamp = Date.now();
+
+        // Note that not all MIDI controllers send a separate NOTE_OFF command for every NOTE_ON.
+        if (cmd === NOTE_OFF || (cmd === NOTE_ON && velocity === 0)) {
+            console.log(`🎧 from ${event.srcElement.name} note off: pitch:${pitch}, velocity: ${velocity}`);
+
+            // Complete the note!
+            const note = notesOn.get(pitch);
+            if (note) {
+                console.log(`🎵 pitch:${pitch}, duration:${timestamp - note} ms.`);
+                notesOn.delete(pitch);
+            }
+        } else if (cmd === NOTE_ON) {
+            console.log(`🎧 from ${event.srcElement.name} note on: pitch:${pitch}, velocity: {velocity}`);
+
+            // One note can only be on at once.
+            //notesOn.set(pitch, timestamp);
+        }
+    }
+
+    onMount(() => {
+        connect();
+
+    })
+</script>
+
+<div>
+    <p>midi aksjdhkassh</p>
+</div>
+
+<style>
+
+</style>
