b989264cd15faec855d5ea81e1f3e45e858d3a54
Damien Sawyer <damiensawyer@gmail.com>
2022-03-08 05:07:36 +0000

    added static site adaptor

--- svelte.config.js
+++ svelte.config.js
@@ -1,2 +1,13 @@
 import {sveltePreprocess} from "svelte-preprocess/dist/autoProcess.js";
- export default {preprocess: sveltePreprocess()}
+import adapter from '@sveltejs/adapter-static';
+ export default {preprocess: sveltePreprocess(),
+     kit: {
+         adapter: adapter({
+             // default options are shown
+             pages: 'build',
+             assets: 'build',
+             fallback: null,
+             precompress: false
+         })
+     }}
+
